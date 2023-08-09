7ad78d672d47413235a72440602645576253cfa1
Максим Самисько <masechk@gmail.com>
2023-08-09 18:29:49 +0300

    Unit25_EFTraining: initial commit

---
+++ Unit25_EFTraining/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Unit25_EFTraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
