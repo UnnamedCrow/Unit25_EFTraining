cb55d190d41406b036b57edff7d601ef698aee7e
Максим Самисько <masechk@gmail.com>
2023-08-09 18:47:57 +0300

    Unit25_EFTrainig: Addad two entities (User,Book) into program and into database (Library)

---
+++ Unit25_EFTraining/AppContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit25_EFTraining
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+        //Объекты таблицы Books
+        public DbSet<Book> Books { get; set; }
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = User-PC\SQLEXPRESS; DataBase = Library; Trusted_Connection = True;Trust Server Certificate = True ");
+        }
+    }
+}
--- Unit25_EFTraining/Program.cs
+++ Unit25_EFTraining/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var db = new AppContext())
+            {
+                var User1 = new User { Name = "Andrey", Email = "An@mail.com" };
+                var Book1 = new Book { Name = "Brain", Created = new DateTime(2008, 3, 1) };
+                db.Users.Add(User1);
+                db.Books.Add(Book1);
+                db.SaveChanges();
+            }
         }
     }
 }
